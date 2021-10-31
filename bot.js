08abf1fed7bd9816090e897fb9ec63a479d17e95
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-10-31 11:35:19 +0500

    WIP

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ const bot = new Telegraf('2091225781:AAEWFmV37CttQZuI7FpODXykeB8o7chCL7M'); // t
 bot.start((ctx) => {
     ctx.reply(ctx.from.first_name + " have entered the start command and it is a " + ctx.updateSubTypes[0]);
     // console.log(ctx);
-    console.log(ctx.from);
-    console.log(ctx.chat);
-    console.log(ctx.message);
-    console.log(ctx.updateSubTypes);
+    // console.log(ctx.from);
+    // console.log(ctx.chat);
+    // console.log(ctx.message);
+    // console.log(ctx.updateSubTypes);
 });
 
 bot.help((ctx) => {
@@ -21,4 +21,19 @@ bot.settings((ctx) => {
     ctx.reply("You have entered settings command");
 });
 
+bot.command(["test", "Test", "test1"], (ctx)=>{
+    ctx.reply("Hello World");
+});
+
+bot.hears("cat", (ctx) => {
+    ctx.reply("Meow");
+});
+
+bot.on("text", (ctx) => {
+    ctx.reply("This is a text message");
+});
+
+bot.on("sticker", (ctx) => {
+    ctx.reply("This is a sticer message");
+});
 bot.launch(); // telegramdagi updatelarni pull qilish uchun ishlatiladi
